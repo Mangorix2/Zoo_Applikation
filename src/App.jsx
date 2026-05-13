b0c788c19ca00d708a3fa0d9c2f4f6d16f359844
Bandnet <andre.bernet@stud.kbw.ch>
2026-05-13 11:20:13 +0200

    feat:Create the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import './App.css';
+import { HashRouter as Router, Routes, Route, NavLink, Link } from 'react-router-dom';
+
+import Startseite from "./pages/Startseite.jsx";
+import Impressum from "./pages/Impressum.jsx";
+import Datenschutz from "./pages/Datenschutz.jsx";
+
+const navClass = ({ isActive }) => isActive ? "active" : "";
 
 function App() {
+    return (
+        <Router>
+            <div>
+                <div className="Navigation">
+                    <nav>
+                        <Link to="/"><h2>MeinProjekt</h2></Link>
+                    </nav>
+                    <nav>
+                        <NavLink to="/" className={navClass} end>Startseite</NavLink>
+                        <NavLink to="/impressum" className={navClass}>Impressum</NavLink>
+                        <NavLink to="/datenschutz" className={navClass}>Datenschutz</NavLink>
+                    </nav>
+                </div>
 
-  return (
-    <div>
-      Hallo
-    </div>
-  )
+                <Routes>
+                    <Route path="/" element={<Startseite/>}/>
+                    <Route path="/impressum" element={<Impressum/>}/>
+                    <Route path="/datenschutz" element={<Datenschutz/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
