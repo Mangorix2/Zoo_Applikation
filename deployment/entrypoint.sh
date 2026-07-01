214b2a6558cecf24aab491f3f3cecac5f2d89b72
Lucien Deforge <125370560+Mangorix2@users.noreply.github.com>
2026-07-01 15:52:13 +0200

    fix: make server run in seperate shell for deployment

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -9,7 +9,7 @@ echo "Installing dependencies"
 npm install
 
 echo "Starting Backend"
-node src/backend/server.js &
+nohup node src/backend/server.js > backend.log 2>&1 &
 
 echo "Starting application"
 exec npm run dev -- --host
