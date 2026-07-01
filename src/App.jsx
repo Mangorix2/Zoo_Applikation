7065880b6b693b3ed6bc227162416265c34ec3f1
Lucien Deforge <125370560+Mangorix2@users.noreply.github.com>
2026-07-01 15:29:02 +0200

    fix: spelling error bc linux/docker is case-sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ZooKarte from "./ZooKarte.jsx";
 import PopupAd from "./PopupAd.jsx";
 import Billetseite from "./Billetseite.jsx";
 import Rating from "./ratingseite.jsx";
-import ZooAiChat from "./ZooAIChat.jsx";
+import ZooAiChat from "./ZooAiChat.jsx";
 
 const navClass = ({ isActive }) => isActive ? "active" : "";
 
